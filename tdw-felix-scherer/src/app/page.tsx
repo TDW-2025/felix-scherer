9ec65c25bbb523e3a1dd209bfceb809ff34bb43f
Félix SCHERER <felix.scherer@laposte.net>
2025-09-22 11:38:32 +0100

    feat: creating a new page

--- tdw-felix-scherer/src/app/page.tsx
+++ tdw-felix-scherer/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello World!</h1>
       <h2>This is the edited page.</h2>
+      <Link href={"/array-map"}>
+        <button>Navigate</button>
+      </Link>
     </div>
   );
 }
