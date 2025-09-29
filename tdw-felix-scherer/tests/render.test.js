692ec04f54244750d08c2a0700981059a523c726
Félix SCHERER <felix.scherer@laposte.net>
2025-09-29 11:12:05 +0100

    lib: added jest + basic tests

---
+++ tdw-felix-scherer/tests/render.test.js
@@ -0,0 +1,15 @@
+// import dependencies
+import React from 'react'
+
+// import react-testing methods
+import {render, fireEvent, screen} from '@testing-library/react'
+
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom'
+
+import Home from '../src/app/page'
+
+test('Checks if Hello World is rendered in homepage', async () => {
+  render(<Home/>)
+  expect(screen.getByText('Hello World!')).toBeInTheDocument()
+})
